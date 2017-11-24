fb5306e96ae47cd3b9af628ce6d8df8d9ad50164
cuongnh <hungcuongqn86@gmail.com>
2017-11-24 08:55:20 +0700

    css

--- resources/views/urldata.blade.php
+++ resources/views/urldata.blade.php
@@ -6,15 +6,23 @@
             <th class="col-md-2 hidden-xs hidden-sm">Created</th>
             <th class="col-md-1 hidden-xs col-sm-2">Short URL</th>
             <th class="col-md-1 col-sm-2 col-xs-3">All Clicks</th>
+            <th class="col-md-1 col-sm-1 hidden-xs"></th>
         </tr>
         </thead>
         <tbody>
         @foreach ($urls as $item)
-            <tr id="{{$item->id}}" short-url="{{url('/')}}/{{ $item->uri }}" original-url="{{ $item->original }}" class="data-row">
-                <td class="col-md-8 col-sm-8 col-xs-9"><a href="{{ $item->original }}">{{ $item->source }}</a></td>
+            <tr id="{{$item->id}}" short-url="{{url('/')}}/{{ $item->uri }}" original-url="{{ $item->original }}"
+                class="data-row">
+                <td class="col-md-8 col-sm-8 col-xs-9 long-url"><a href="{{ $item->original }}">{{ $item->source }}</a></td>
                 <td class="col-md-2 hidden-xs hidden-sm">{{ $item->created }}</td>
-                <td class="col-md-1 hidden-xs col-sm-2"><a href="{{url('/')}}/{{ $item->uri }}">{{ $item->uri }}</a></td>
-                <td class="col-md-1 col-sm-2 col-xs-3"><a class="a-analytics" href="{{url('/')}}/analytics/{{ $item->uri }}/all_time">{{ sizeof($item->Logs) }}</a></td>
+                <td class="col-md-1 hidden-xs col-sm-2"><a href="{{url('/')}}/{{ $item->uri }}">{{ $item->uri }}</a>
+                </td>
+                <td class="col-md-1 col-sm-2 col-xs-3"><a class="a-analytics"
+                                                          href="{{url('/')}}/analytics/{{ $item->uri }}/all_time">{{ sizeof($item->Logs) }}</a>
+                </td>
+                <td class="col-md-1 col-sm-1 hidden-xs">
+                    <a><i class="glyphicon glyphicon-option-vertical"></i></a>
+                </td>
             </tr>
         @endforeach
         </tbody>
