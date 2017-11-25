f0788bbe434491d75e7c9ac8b64525fd3bc1bfff
cuongnh <hungcuongqn86@gmail.com>
2017-11-25 08:40:48 +0700

    log

--- app/Url.php
+++ app/Url.php
@@ -31,34 +31,6 @@ class Url extends Model
 
     public function getCreatedAttribute()
     {
-        if (isset($this->attributes['created_at']) && $this->attributes['created_at'] !== '') {
-
-            $datetime1 = new \DateTime($this->attributes['created_at']);
-            $datetime2 = new \DateTime();
-            $interval = $datetime1->diff($datetime2);
-            if($interval->y){
-                return $interval->y.' years ago';
-            }
-            if($interval->m){
-                return $interval->m.' months ago';
-            }
-
-            if($interval->d){
-                return $interval->d.' days ago';
-            }
-
-            if($interval->h){
-                return $interval->h.' hours ago';
-            }
-
-            if($interval->i){
-                return $interval->i.' minutes ago';
-            }
-
-            if($interval->s){
-                return 'in 1 minute';
-            }
-        }
-        return 'in 1 minute';
+        return $this->created_at->diffForHumans();
     }
 }
